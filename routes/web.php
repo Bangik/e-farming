465452e8150d99a7e22d5627b20cf177f00aa2d4
Achmad Dinofaldi Firmansyah <47439247+Bangik@users.noreply.github.com>
2023-03-24 07:39:09 +0800

    update permission route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\LaporanPanenController;
 use App\Http\Controllers\ObathController;
 use App\Http\Controllers\PaketTanamController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,35 +27,19 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 Route::group(['middleware' => ['auth', 'admin']], function(){
-    Route::resource('lahan', LahanController::class, ['except' => ['index','show']]);
     Route::resource('user', UserController::class);
-    Route::resource('alatb', Alat_BahanController::class, ['except' => ['index','show','update']]);
-    Route::resource('obath', ObathController::class, ['except' => ['index','show','update']]);
-    Route::resource('paket-tanam', PaketTanamController::class, ['except' => ['index','show']]);
-    Route::resource('laporan', LaporanPanenController::class, ['except' => ['index','show']]);
-    Route::resource('jadwal-pupuk', JadwalPupukController::class, ['except' => ['index','show']]);
-    //Update Alat Bahan
-    Route::post('alatb2/update/{id}', 'App\Http\Controllers\Alat_BahanController@update');
-    //Update Obat Hama
-    Route::post('obath2/update/{id}', 'App\Http\Controllers\ObathController@update');
 });
 
-// Route user
-Route::get('/lahan', [LahanController::class, 'index'])->name('lahan.index');
-Route::get('/lahan/{lahan}', [LahanController::class, 'show'])->name('lahan.show');
-Route::get('alatb', [Alat_BahanController::class, 'index'])->name('alatb.index');
-Route::get('alatb/{alatb}', [Alat_BahanController::class, 'show'])->name('alatb.show');
-Route::get('obath', [ObathController::class, 'index'])->name('obath.index');
-Route::get('obath/{obath}', [ObathController::class, 'show'])->name('obath.show');
-Route::get('paket-tanam', [PaketTanamController::class, 'index'])->name('paket-tanam.index');
-Route::get('paket-tanam/{paket_tanam}', [PaketTanamController::class, 'show'])->name('paket-tanam.show');
-Route::get('laporan', [LaporanPanenController::class, 'index'])->name('laporan.index');
-Route::get('laporan/{laporan}', [LaporanPanenController::class, 'show'])->name('laporan.show');
-Route::get('jadwal-pupuk', [JadwalPupukController::class, 'index'])->name('jadwal-pupuk.index');
-Route::get('jadwal-pupuk/{jadwal_pupuk}', [JadwalPupukController::class, 'show'])->name('jadwal-pupuk.show');
-Route::get('ganti-password', [HomeController::class, 'change_password'])->middleware('auth')->name('ganti-password.index');
-Route::post('ganti-password', [HomeController::class, 'update_password'])->middleware('auth')->name('ganti-password.update');
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('lahan', LahanController::class);
+    Route::resource('alatb', Alat_BahanController::class);
+    Route::resource('obath', ObathController::class);
+    Route::resource('paket-tanam', PaketTanamController::class);
+    Route::resource('laporan', LaporanPanenController::class);
+    Route::resource('jadwal-pupuk', JadwalPupukController::class);
+    Route::get('ganti-password', [HomeController::class, 'change_password'])->name('ganti-password.index');
+    Route::post('ganti-password', [HomeController::class, 'update_password'])->name('ganti-password.update');
+});
 
